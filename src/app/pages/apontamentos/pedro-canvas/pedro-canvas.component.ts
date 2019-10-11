3fdd638323711ab98e2f8086d0b0eab076b92e5f
Pedro Alcântara <pfalcantara@stefanini.com>
2019-10-11 00:51:26 -0300

    remove new charts update the old ones

--- src/app/pages/apontamentos/pedro-canvas/pedro-canvas.component.ts
+++ src/app/pages/apontamentos/pedro-canvas/pedro-canvas.component.ts
@@ -1,142 +1,93 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, BaseChartDirective, Label } from 'ng2-charts';
-import * as pluginAnnotations from 'chartjs-plugin-annotation';
+import { Component, OnInit, ViewChild, AfterViewInit, SimpleChanges } from "@angular/core";
+import { ChartDataSets, ChartOptions } from "chart.js";
+import { Color, BaseChartDirective, Label } from "ng2-charts";
+import * as pluginAnnotations from "chartjs-plugin-annotation";
 
 @Component({
-  selector: 'ngx-pedro-canvas',
-  templateUrl: './pedro-canvas.component.html',
-  styleUrls: ['./pedro-canvas.component.scss']
+  selector: "ngx-pedro-canvas",
+  templateUrl: "./pedro-canvas.component.html",
+  styleUrls: ["./pedro-canvas.component.scss"]
 })
 export class PedroCanvasComponent implements OnInit {
-
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+  public lineChartData: ChartDataSets[] = [{ data: [8, 8, 4, 5, 10, 2, 4, 4] }];
+  public lineChartLabels: Label[] = Array(31)
+    .fill(0)
+    .map((x, i) => "");
+  public lineChartOptions: ChartOptions = {
+    maintainAspectRatio: false,
     responsive: true,
+    // animation: {
+    //   onComplete: function() {
+    //     var ctx = this.chart.ctx;
+    //     ctx.textAlign = "center";
+    //     ctx.textBaseline = "middle";
+    //     var chart = this;
+    //     var datasets = this.config.data.datasets;
+
+    //     datasets.forEach(function(dataset: Array<any>, i: number) {
+    //       ctx.font = "10px Arial";
+
+    //       ctx.fillStyle = "Black";
+    //       chart.getDatasetMeta(i).data.forEach(function(p: any, j: any) {
+    //         ctx.fillText(datasets[i].data[j], p._model.x, p._model.y - 10);
+    //       });
+    //     });
+    //   }
+    // },
+    tooltips: {
+      enabled: true
+    },
+    legend: {
+      display: false
+    },
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
-      xAxes: [{}],
       yAxes: [
         {
-          id: 'y-axis-0',
-          position: 'left',
-        },
-        {
-          id: 'y-axis-1',
-          position: 'right',
-          gridLines: {
-            color: 'rgba(255,0,0,0.3)',
-          },
-          ticks: {
-            fontColor: 'red',
-          }
+          display: false,
+          scaleLabel: { display: false, fontSize: 0 },
+          ticks: { padding: 50 }
         }
-      ]
-    },
-    annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
-          }
-        },
       ],
-    },
+      xAxes: [{ display: false, ticks: { padding: 50 } }]
+    }
   };
   public lineChartColors: Color[] = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // red
-      backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'red',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    {
+      // grey
+      backgroundColor: "rgba(148,159,177,0.2)",
+      borderColor: "rgba(148,159,177,1)",
+      pointBackgroundColor: "rgba(148,159,177,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(148,159,177,0.8)"
     }
   ];
-  public lineChartLegend = true;
-  public lineChartType = 'line';
-  public lineChartPlugins = [pluginAnnotations];
+  public lineChartLegend = false;
+  public lineChartType = "line";
 
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
-  constructor() { }
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log(changes);
+
+  }
 
   ngOnInit() {
   }
 
-  public randomize(): void {
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        this.lineChartData[i].data[j] = this.generateNumber(i);
-      }
-    }
-    this.chart.update();
-  }
-
-  private generateNumber(i: number) {
-    return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
-  }
-
   // events
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  public chartClicked({
+    event,
+    active
+  }: {
+    event: MouseEvent;
+    active: {}[];
+  }): void {
     console.log(event, active);
   }
-
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public hideOne() {
-    const isHidden = this.chart.isDatasetHidden(1);
-    this.chart.hideDataset(1, !isHidden);
-  }
-
-  public pushOne() {
-    this.lineChartData.forEach((x, i) => {
-      const num = this.generateNumber(i);
-      const data: number[] = x.data as number[];
-      data.push(num);
-    });
-    this.lineChartLabels.push(`Label ${this.lineChartLabels.length}`);
-  }
-
-  public changeColor() {
-    this.lineChartColors[2].borderColor = 'green';
-    this.lineChartColors[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
-  }
-
-  public changeLabel() {
-    this.lineChartLabels[2] = ['1st Line', '2nd Line'];
-    // this.chart.update();
-  }
-
 }
